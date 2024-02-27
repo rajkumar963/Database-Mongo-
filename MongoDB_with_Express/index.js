0c8371ba1c2917670aadd34a7c74f1bea4485809
rajkumar963 <rajkumarsaw597@gmail.com>
2024-02-28 02:02:59 +0530

    new update

--- MongoDB_with_Express/index.js
+++ MongoDB_with_Express/index.js
@@ -3,10 +3,13 @@ const app=express();
 const mongoose=require("mongoose");
 const path=require("path");
 const Chat=require("./models/chat.js");
+const methodOverride=require("method-override");
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 main()
 .then(() =>{
@@ -21,8 +24,66 @@ async function main() {
 
 app.get("/chats",async(req,res)=>{
    let chats=await Chat.find();
-   console.log(chats);
+   //console.log(chats);
    res.render("index.ejs",{chats});
+  }); 
+
+//New Route:------
+   app.get("/chats/new",(req,res)=>{
+       res.render("new.ejs");
+   });
+
+//Create Route:----
+  app.post("/chats",(req,res)=>{
+      let {from,to,message}=req.body;
+      let newChat=new Chat({
+          from:from,
+          to:to,
+          message:message,
+          created_at:new Date()
+
+      })
+      newChat
+      .save()       //jaha then use karte hai waha await use nahi karte hai(asynchronous)
+      .then(()=>{
+        console.log("saved");
+         })
+        .catch((err)=>{
+            console.log(err)
+        });
+      res.redirect("/chats");
+        });
+
+    // Edit Route:----
+    
+    app.get("/chats/:id/edit",async(req,res)=>{
+        let{id}=req.params;
+        let chat=await Chat.findById(id);
+        res.render("edit.ejs",{chat});
+    })
+
+    //Update Route:----
+
+    app.put("/chats/:id",async(req,res)=>{
+        let{id}=req.params;
+        let {message: newmessage}=req.body;
+        console.log(newmessage);
+        let updatedChat=await Chat.findByIdAndUpdate(
+            id,{message: newmessage},
+            {runValidators:true, new:true});
+        console.log(updatedChat);
+        res.redirect("/chats");
+    });
+
+
+    //Delete Route:----
+
+    app.delete("/chats/:id",async(req,res)=>{
+        let{id}=req.params;
+       let deletedChat= await Chat.findByIdAndDelete(id);
+       console.log(deletedChat);
+       res.redirect("/chats");
+    })
 
     // .then((result)=>{
     //     res.render("index",{chats:result});
@@ -30,7 +91,7 @@ app.get("/chats",async(req,res)=>{
     // .catch((err)=>{
     //     console.log(err);
     // })
-});
+
 
             // let chat1=new Chat({
             //     from:"Rajkumar",
